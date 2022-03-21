66048e4537473bde89eb9170d5966a5b80fad6eb
befd704a42cea8c <101062813+6d33a40765b20c6@users.noreply.github.com>
2022-03-21 13:58:12 +0300

    dev
     + Добавил возможность получить код для входа или вода my.telegram.org и другое от бота тг
    + Пофиксил ошибки

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from datetime import datetime as dt
 import easygui
 from colorama import Fore, init
 from opentele.api import UseCurrentSession
+from telethon import events
 from opentele.td import TDesktop
 from tqdm import tqdm
 
@@ -27,7 +28,6 @@ bb  = f'''
 '''
 print(C.MAGENTA+bb)
 async def save_dialogs(id,client):
-    if not os.path.exists(id):os.mkdir(id)
     if not os.path.exists(id+'/dialogs'):os.mkdir(id+'/dialogs')
     dialogs = {'dialogID': {'name': 'chatName','countMessage': 999,'countLinks':999,'countMails': 999}}
     links = {'dialogID': ['links','links','links']}
@@ -43,11 +43,18 @@ async def save_dialogs(id,client):
     if dialogs != {'dialogID': {'name': 'chatName','countMessage': 999,'countLinks':999,'countMails': 999}}:print(json.dumps(dialogs,indent=4,ensure_ascii=False),file=codecs.open(id+'/dialogs/dialogs.json','w','utf8'))
     if links != {'dialogID': ['links','links','links']}: print(json.dumps(links,indent=4,ensure_ascii=False),file=codecs.open(id+'/dialogs/links.json','w','utf8'))
     if mails != {'dialogID': ['mails','mails']}: print(json.dumps(mails,indent=4,ensure_ascii=False),file=codecs.open(id+'/dialogs/mails.json','w','utf8'))
+async def get_sms(phone,client):
+    print(C.GREEN+'Login telegram with phone:',C.MAGENTA+phone)
+    @client.on(events.NewMessage(chats=(777000)))
+    async def normal_handler(event):
+        print(C.RESET+event.message.to_dict()['message'])
+    await client.run_until_disconnected()
+
 async def main(tdataFolder):
     try:tdesk = TDesktop(tdataFolder);assert tdesk.isLoaded()
     except:raise Exception('"tdata" no valid')
-    if not os.path.exists('sessions'):os.mkdir('sessions')
     id_user = str(tdesk.mainAccount.UserId)
+    if not os.path.exists(id_user):os.mkdir(id_user)
     client = await tdesk.ToTelethon(session=id_user+'/'+id_user+"-ttot.session", flag=UseCurrentSession)    
     await client.connect()
     user = await client.get_me()
@@ -60,6 +67,7 @@ async def main(tdataFolder):
     print(json.dumps(userInfo,indent=4,ensure_ascii=False),file=codecs.open(id_user+'/user.json','w','utf8'))
     di = str(len(await client.get_dialogs()))
     if input(C.GREEN+f'Найдено {di} диалогов.\nХотите сохранить? (Y-да;другое-нет): '+C.BLUE).lower() == 'y':await save_dialogs(id_user,client)
+    if input(C.GREEN+f'Хотите получить смс чтобы войти через обычный телеграмм? (После получения нажмите ctrl+c) (Y-да;другое-нет): '+C.BLUE).lower() == 'y':await get_sms(userInfo['phone'],client)
     print(C.GREEN+'Результат в папке - '+C.MAGENTA+ id_user)
     await client.disconnect()
 try:
